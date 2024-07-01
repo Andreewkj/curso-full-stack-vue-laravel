222da0900a9d9e0626babb5306ac4b738660d4fb
andreewkj <andreew.k.januario@outlook.com>
2024-06-30 21:01:12 -0300

    first commit

--- src/App.vue
+++ src/App.vue
@@ -2,23 +2,9 @@
     <Nav/>
 
     <router-view></router-view>
-    <input type="text" name="" id="" placeholder="search" @keyup="search">
-    <button>search</button>
 
 </template>
 
 <script setup>
 import Nav from '@/components/Nav.vue';
-import _ from 'lodash';
-
-import { ref, onMounted, reactive } from 'vue';
-
-const search = _.debounce(() => {
-  console.log('search');
-}, 1000)
-
-// function search() {
-//   console.log('search');
-// }
-
 </script>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,24 +1,55 @@
 <template>
 <div>
-    <button @click="openModal = true">Abrir</button>
-    
-    <Modal v-if="openModal" @close-modal="openModal = false">
-        <template #default>
-            <form action="" method="get">
-                <input type="text" name="" placeholder="Email">
-                <input type="text" name="" placeholder="Senha">
-                <button>Login</button>
-            </form>
-        </template>
-    </Modal>
+    <input type="text" v-model="user.firstName" placeholder="firstName">
+    <input type="text" v-model="user.lastName" placeholder="lastName">
+
+    <ul>
+        <li v-for="item in items" :key="item.id">{{ item.name }} <input type="text" v-model="item.name"></li>
+    </ul>
 </div>
 </template>
 
 <script setup>
 
-import {ref} from 'vue';
-import Modal from '@/components/Modal.vue'
+import {ref, watch, reactive} from 'vue';
 
-const openModal = ref(false);
+const firstName = ref('');
+const lastName = ref('');
+
+watch([firstName, lastName], ([valueFirstName, valueLastName], [oldValueFirstName, oldValueLastName]) => {
+    console.log('new => ' + valueFirstName, 'old => ' + oldValueFirstName);
+})
+
+const user = reactive({
+    firstName:'',
+    lastName:''
+})
+
+const items = reactive([
+    {
+        id: 1,
+        name:'Alexandre'
+    },
+    {
+        id:2,
+        name:'João'
+    },
+])
+
+// watch(() => 
+//     user.firstName,
+//         (value, oldValue) => {
+//             console.log(value + '-' + oldValue);
+//         }
+//     )
+
+watch(
+    items, (value) => {
+        value.forEach((item) => {
+        if (!Number.isInteger(Number(item))) {
+            console.log('not a number '+item.name);
+        }
+    })
+})
 </script>
 
